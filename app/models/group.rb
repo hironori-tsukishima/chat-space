343f4bab54e803b01d8d6b09544978c7041ef7f2
tsukishima <hir01994o915@gmail.com>
2018-11-14 09:34:11 +0900

    アソシエーションの実行

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+
+  has_many :users , through: :users_groups_relations
+  has_many :users_groups_relations
+  has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :groups, through: :users_groups_relations
+  has_many :users_groups_relations
+  has_many :messages
 end
