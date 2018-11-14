db982d09f15c46545a862f9353d082e56d3c0e2d
tsukishima <hir01994o915@gmail.com>
2018-11-14 20:25:05 +0900

    中間テーブルに値が入らなかったので修正（null:falseが諸悪の根源）

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20181114002319) do
   end
 
   create_table "users_groups_relations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "group_id",   null: false
+    t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_users_groups_relations_on_group_id", using: :btree
