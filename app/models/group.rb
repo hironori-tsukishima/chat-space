dcd57cbcdfd61b4c1ca2929ce25b35e75c9f7a24
tsukishima <hir01994o915@gmail.com>
2018-11-15 21:58:12 +0900

    サイドバーに表示されるメッセージの表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :users , through: :users_groups_relations
   has_many :users_groups_relations
   has_many :messages
+
+  def showLastMessage
+    if (content = messages.last).present?
+      if content.text?
+        content.text
+      else
+        "画像が送信されています"
+      end
+    else
+      "まだメッセージはありません"
+    end
+  end
 end
