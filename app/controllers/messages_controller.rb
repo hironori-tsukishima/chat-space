ed0847d4bfbcfb8fdd2295d6a9efcecc6ad3d581
tsukishima <hir01994o915@gmail.com>
2018-11-15 21:57:37 +0900

    Create、indexアクションの定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,35 @@
 class MessagesController < ApplicationController
 
-  def index
-  end
+  before_action :set_group
 
-  def create
-  end
+    def index
+      @message = Message.new
+      @messages = @group.messages.includes(:user)
+    end
 
-  def edit
-  end
+    def create
+      @message = @group.messages.new(message_params)
+      if @message.save
+        redirect_to group_messages_path(@group), notice:"チャット成功！"
+      else
+        @messages = @group.messages.includes(:user)
+        flash.now[:alert] = "送れてないよ。画像かテキストは入れてな"
+        render :index
+      end
+    end
 
-  def update
-  end
+    def edit
+    end
 
+    def update
+    end
+
+    def message_params
+      params.require(:message).permit(:image , :text).merge(user_id: current_user.id)
+    end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 
 end
