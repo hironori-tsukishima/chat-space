6a1019175e52bb97eff5d1a1f667bef91a6e67a2
tsukishima <hir01994o915@gmail.com>
2018-11-27 20:56:29 +0900

    Imageの保存先の変更

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     text              Faker::HarryPotter.quote
-    image             File.open("#{Rails.root}/public/uploads/message/image/5/no_image.jpg")
+    image             File.open("#{Rails.root}/public/images/no_image.jpg")
     user_id              10
     group_id             10
     # user_id           rand(10)
