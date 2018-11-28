6dbb26b0605637e75d74892dd49f27cc14ba836c
tsukishima <hir01994o915@gmail.com>
2018-11-28 17:39:12 +0900

    記述変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '54.64.15.106', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -7,7 +8,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '54.64.15.106', user: 'ec2-user', roles: %w{app db web}
+
 
 # role-based syntax
 # ==================
